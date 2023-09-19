a35294fa0092b7f1e2fca7492f80e31273150b46
zikoo <rikott98@gmail.com>
2023-09-19 17:34:03 +0300

    new projectt islamii app and make a first part of it

---
+++ lib/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+static const String routeName = "Home-Screen";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
